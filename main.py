8abde8a49cf545c81798a39d6dee8844d78175f3
Danil <ferrum268@mail.ru>
2023-11-02 17:36:22 +0500

    write argparse

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
+import argparse
+from os.path import isfile
+
+
 def main():
-    pass
+    parser = argparse.ArgumentParser(description="Deflate")
+    parser.add_argument(
+        "path", type=str,
+        help="Absolute path to a file to be compressed",
+    )
+    parser.add_argument(
+        "action", type=str,
+        choices=["compress", "decompress"],
+        help="Whether to compress or decompress given file"
+    )
+    args = parser.parse_args()
+    if path_correct(args.path):
+        pass
+    else:
+        raise AttributeError(f"Incorrect path: {args.path}")
 
 
-if __name__ == "main":
+def path_correct(path):
+    return isfile(path)
+
+
+if __name__ == "__main__":
     main()

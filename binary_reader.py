4b6b244e5d388efb11b537a2b0316f924e114b03
Danil <ferrum268@mail.ru>
2023-11-13 21:19:49 +0500

    write some more tests for binary_reader.py

--- binary_reader.py
+++ binary_reader.py
@@ -2,10 +2,15 @@ import bitstring
 
 
 class BinaryReader:
-    def __init__(self, path: str):
-        with open(path, "rb") as file:
-            self._data = bitstring.Bits(file).bin
+    def __init__(self, data: str):
+        self._data = data
         self._ptr: int = 0
+
+    @staticmethod
+    def read_file(path: str):
+        with open(path, "rb") as file:
+            data = bitstring.Bits(file).bin
+        return BinaryReader(data)
 
     def next_bit(self) -> str:
         if self._ptr >= len(self._data):

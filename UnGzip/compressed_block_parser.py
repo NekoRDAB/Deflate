4d6e0d051a80acf758b3cf5cac94c753897bfca7
Danil <ferrum268@mail.ru>
2023-11-14 15:50:53 +0500

    renaming in compressed_block_parser.py

--- UnGzip/compressed_block_parser.py
+++ UnGzip/compressed_block_parser.py
@@ -7,9 +7,9 @@ class CompressedBlockParser:
     def parse_block(bit_stream: BitStreamReader) -> bformat.BlockFormat:
         is_last = bit_stream.next_bit()
         btype = bit_stream.next_bits(2)
-        data = CompressedBlockParser.parse_data(btype, bit_stream)
-        return bformat.BlockFormat.create_block(is_last, btype, data)
+        body = CompressedBlockParser.parse_body(btype, bit_stream)
+        return bformat.BlockFormat.create_block(is_last, btype, body)
 
     @staticmethod
-    def parse_data(btype: str, bit_stream: BitStreamReader):
+    def parse_body(btype: str, bit_stream: BitStreamReader):
         raise NotImplementedError()
